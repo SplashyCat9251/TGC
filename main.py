0d471d8b28752f2c318ecb615ac85133bac0ad03
JanitorMiguel <125312552+SplashyCat9251@users.noreply.github.com>
2023-02-13 21:17:25 +0000

    -Fixed Line 1 -Added Line 3 through 6

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("turns out it was a normal brownie")
+print("Turns out it was a normal brownie")
+
+for i in range(5):
+  print("And so he got another brownie")
+
+print("It was infact, not a normal brownie")
